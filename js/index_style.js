0b1bb9f659211605629ffec67fa956aeed832457
shinjuno <shinjuno123@naver.com>
2022-01-24 10:53:41 +0900

    add video number control function

--- js/index_style.js
+++ js/index_style.js
@@ -1,5 +1,5 @@
-div = document.querySelector('main div')
-
+const mainDiv = document.querySelector('main div')
+const NUM_OF_VIDEOS = 20;
 const videoFleet = () =>{
     let videoFleet = document.createElement('div');
     if(videoFleet){
@@ -8,13 +8,34 @@ const videoFleet = () =>{
     }
 } 
 const videoBox = () => {return`
-    <div class="video-box">)
+    <div class="video-box">
         <img src="./image/video_thumbnail_tmp.png" alt="video-thumbnail">
     </div>
     `;
 }
 
-console.log(videoFleet());
+function ListVideo(count){
+    let videoArr = [];
+    for(let i=0;i<count;i++){
+        if(videoArr.length % 4 === 0){
+            let videoFleetTmp = videoFleet();
+            videoArr.map((value)=>{
+                videoFleetTmp.innerHTML += value
+            })
+            mainDiv.appendChild(videoFleetTmp);
+            videoArr = [];
+        }
+        videoArr.push(videoBox());
+        
+    }
+    let videoFleetTmp = videoFleet();
+    videoArr.map((value)=>{
+        videoFleetTmp.innerHTML += value;
+    })
+    mainDiv.appendChild(videoFleetTmp);
+}
+
+ListVideo(NUM_OF_VIDEOS);
 
 
 
